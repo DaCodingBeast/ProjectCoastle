5072aa69f6e6aef315951af25ffb819f115c72a7
DaCodingBeast <146308828+DaCodingBeast@users.noreply.github.com>
2025-07-18 00:45:39 -0700

    Moved backend to server (in  a private repo)

--- Project_Coastle/TransformData.py
+++
@@ -1,5 +0,0 @@
-# Class that can take in an array, or entire csv file and transform each sample based on a data anlysis, that finds the degree of polynomial regression I want to fit the features to.
-
-
-
-
